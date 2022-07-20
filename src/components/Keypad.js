e16f24d49496073379018882b14f61c8549b5f9d
Sean Oh <ohsean812@gmail.com>
2022-07-19 20:46:02 -0400

    Done with lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function pressKey (){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input
+            type="password"
+            onChange={pressKey}
+            />
+        </div>
     )
 }
 
